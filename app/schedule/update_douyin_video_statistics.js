a7fbb1c8a359037ff5df07e1ef714990b4a08fc0
GiantDwarf <971790583@qq.com>
2021-04-27 15:08:17 +0800

    :art: 调整定时任务;

--- app/schedule/update_douyin_video_statistics.js
+++ app/schedule/update_douyin_video_statistics.js
@@ -2,28 +2,34 @@
  * @Author: zhujian1995@outlook.com
  * @Date: 2021-04-25 14:36:56
  * @LastEditors: zhujian
- * @LastEditTime: 2021-04-27 13:42:21
+ * @LastEditTime: 2021-04-27 15:07:36
  * @Description: 你 kin 你擦
  */
 'use strict';
 
 const Subscription = require('egg').Subscription;
+const moment = require('moment');
 
 class UpdateStatistics extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      interval: '1h',
+      interval: '1m',
       type: 'worker',
       env: ['prod'],
+      immediate: true,
     };
   }
 
   // subscribe 是真正定时任务执行时被运行的函数
   async subscribe() {
-    console.log('执行更新');
-    const ctx = await this.app.createAnonymousContext();
-    await ctx.service.dyVideo.updateAllVideos();
+    const nowMinutes = moment().format('mm');
+    // 每小时更新一次
+    if (nowMinutes === '35') {
+      console.log('执行视频更新');
+      const ctx = await this.app.createAnonymousContext();
+      await ctx.service.dyVideo.updateAllVideos();
+    }
   }
 }
 
