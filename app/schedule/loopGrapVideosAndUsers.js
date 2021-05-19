50496a9265cefb003c9cb3527ee8c82f01a62627
GiantDwarf <971790583@qq.com>
2021-05-19 14:28:38 +0800

    :art: 定时任务调整

--- app/schedule/loopGrapVideosAndUsers.js
+++ app/schedule/loopGrapVideosAndUsers.js
@@ -2,13 +2,12 @@
  * @Author: zhujian1995@outlook.com
  * @Date: 2021-05-14 17:12:08
  * @LastEditors: zhujian
- * @LastEditTime: 2021-05-14 17:23:48
+ * @LastEditTime: 2021-05-19 14:28:14
  * @Description: 你 kin 你擦
  */
 'use strict';
 
 const Subscription = require('egg').Subscription;
-const moment = require('moment');
 
 class LoopGrapVideosAndUsers extends Subscription {
   static get schedule() {
@@ -21,12 +20,8 @@ class LoopGrapVideosAndUsers extends Subscription {
   }
 
   async subscribe() {
-    // 避开另一个定时任务的请求密集期每小时的第10分钟-第20分钟，这时间段内不请求
-    const nowMinutes = moment().get('minutes');
-    if (nowMinutes < 9 || nowMinutes > 20) {
-      const ctx = await this.app.createAnonymousContext();
-      ctx.service.dyVideo.getRecommendedAweme();
-    }
+    const ctx = await this.app.createAnonymousContext();
+    ctx.service.dyVideo.getRecommendedAweme();
   }
 }
 
